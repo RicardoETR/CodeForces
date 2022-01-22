d5ff7ba449bd4e8baca8975b5f8b5a79cede492a
Ricardo Torres <r.erick.torres.r@gmail.com>
2022-01-22 03:05:54 -0600

    watermelon

---
+++ watermelon.cpp
@@ -0,0 +1,17 @@
+//Autor: RicardoETR.
+//4A Watermelon
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int kg;
+    cin >> kg;
+    if (kg%2==0 && kg > 2)
+    {
+        cout << "YES";
+    }
+    else{
+        cout << "NO";
+    }
+    return 0;
+}
