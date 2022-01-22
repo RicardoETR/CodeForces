4fd52425788d7d74f7c30b3cc0926878bdfad0b2
Ricardo Torres <r.erick.torres.r@gmail.com>
2022-01-22 04:16:28 -0600

    domino piling

---
+++ dominoPiling.cpp
@@ -0,0 +1,11 @@
+//Autor: Ricardo Erick Torres Rosas.
+//50A Domino Piling
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+    cout << (m*n) / 2;
+    return 0;
+}
